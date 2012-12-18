68e3a8949a5dae9870b43b0f2d2cb77d8649cb6a
lemonhall <lemonhall@gmail.com>
2012-12-18 10:17:30 +0800

    first blood

---
+++ app.js
@@ -0,0 +1,17 @@
+var resourceful = require('resourceful'),
+    Tasks    = resourceful.define('tasks');
+
+
+
+resourceful.use('memory');
+
+Tasks.property('type', String, { default: "dragon" });
+Tasks.property('life', Number, { default: 10, minimum: 0, maximum: 20 });
+
+var restful     = require('restful');
+//
+// Setup a standalone restful server
+//
+var server = restful.createServer(Tasks);
+server.listen(8000);
+console.log(' > http server started on port 8000');
